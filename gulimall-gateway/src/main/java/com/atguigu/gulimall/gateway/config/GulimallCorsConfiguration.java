e68a173990b2f11b74df4c010c0bcf077b28d8dc
christ <251495054@qq.com>
2023-10-27 03:23:59 +0800

    202310.27

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.atguigu.gateway.config;
+package com.atguigu.gulimall.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,20 +7,21 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
-public class gulimall_CorsConfiguration {
+public class GulimallCorsConfiguration {
+
     @Bean
-    public CorsWebFilter corswebFilter(){
+    public CorsWebFilter corsWebFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
+        //1、配置跨域
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.setAllowCredentials(true);
 
-
         source.registerCorsConfiguration("/**",corsConfiguration);
-
-       return new CorsWebFilter(source);
+        return new CorsWebFilter(source);
     }
 }
